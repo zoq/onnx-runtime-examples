5716595a6cacc34cd41e13bf5d8d522f5d003d79
Marcus Edel <marcus.edel@fu-berlin.de>
2021-03-23 18:28:34 +0100

    Use the correct image format.

--- onnx-detection.cpp
+++ onnx-detection.cpp
@@ -1,7 +1,8 @@
 #include <cpu_provider_factory.h>
 // #include <cuda_provider_factory.h>
 
-#include <onnxruntime_cxx_api.h>
+/* #include <onnxruntime_cxx_api.h> */
+#include <experimental_onnxruntime_cxx_api.h>
 
 #include <cmath>
 #include <exception>
@@ -12,9 +13,31 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <cstdint>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+
+std::string print_shape(const std::vector<int64_t>& v) {
+  std::stringstream ss("");
+  for (size_t i = 0; i < v.size() - 1; i++)
+    ss << v[i] << "x";
+  ss << v[v.size() - 1];
+  return ss.str();
+}
+
+int YOLO_GRID_X = 13;
+int YOLO_GRID_Y = 13;
+int YOLO_NUM_BB = 5;
+
+double anchors[] =
+{
+  1.08, 1.19,
+  3.42, 4.41,
+  6.63, 11.38,
+  9.42, 5.11,
+  16.62, 10.52
+};
 
 class Image
 {
@@ -46,6 +69,138 @@ class Image
     int img_w;
     int img_c;
 };
+
+int offset_(int b, int y, int x, const size_t classes)
+{
+  return b * (classes + 5) * YOLO_GRID_X * YOLO_GRID_Y + y * YOLO_GRID_X + x;
+}
+
+int offset(int o, int channel)
+{
+  return o + channel * YOLO_GRID_X * YOLO_GRID_Y;
+}
+
+double Sigmoid(double x)
+{
+  return 1.0 / (1.0 + exp(-x));
+}
+
+void Softmax(float val[], const size_t classes)
+{
+  float max = -INT_MAX;
+  float sum = 0;
+
+  for (size_t i = 0; i < classes; ++i)
+    max = std::max(max, val[i]);
+
+  for (size_t i = 0; i < classes; ++i)
+  {
+    val[i] = (float) exp(val[i] - max);
+    sum += val[i];
+  }
+
+  for (size_t i = 0; i < classes; ++i)
+    val[i] = val[i] / sum;
+}
+
+typedef struct
+{
+  float x;
+  float y;
+  float w;
+  float h;
+} Box;
+
+typedef struct detection
+{
+  Box bbox;
+  float conf;
+  int c;
+  float prob;
+} detection;
+
+typedef struct
+{
+ float dx;
+ float dy;
+ float dw;
+ float dh;
+} Dbox;
+
+Box FloatToBox(const float fx, const float fy, const float fw, const float fh)
+{
+  Box b;
+  b.x = fx;
+  b.y = fy;
+  b.w = fw;
+  b.h = fh;
+
+  return b;
+}
+
+float Overlap(float x1, float w1, float x2, float w2)
+{
+  float l1 = x1 - w1 / 2;
+  float l2 = x2 - w2 / 2;
+  float left = l1 > l2 ? l1 : l2;
+
+  float r1 = x1 + w1 / 2;
+  float r2 = x2 + w2 / 2;
+  float right = r1 < r2 ? r1 : r2;
+
+  return right - left;
+}
+
+float BoxIntersection(const Box& a, const Box& b)
+{
+  float w = Overlap(a.x, a.w, b.x, b.w);
+  float h = Overlap(a.y, a.h, b.y, b.h);
+
+  if(w < 0 || h < 0)
+    return 0;
+
+  return w * h;
+}
+
+float BoxUnion(const Box& a, const Box& b)
+{
+  return a.w * a.h + b.w * b.h - BoxIntersection(a, b);
+}
+
+float BoxIOU(const Box& a, const Box& b)
+{
+  return BoxIntersection(a, b) / BoxUnion(a, b);
+}
+
+void FilterBoxesNMS(std::vector<detection>& det, int nBoxes, float th_nms)
+{
+  int count = nBoxes;
+  for (size_t i = 0;i < count; ++i)
+  {
+    Box a = det[i].bbox;
+    for (size_t j = 0; j < count; ++j)
+    {
+      if (i == j) continue;
+      if (det[i].c != det[j].c) continue;
+
+      Box b = det[j].bbox;
+      float b_intersection = BoxIntersection(a, b);
+      if (BoxIOU(a, b) > th_nms ||
+          b_intersection >= a.h * a.w - 1 ||
+          b_intersection >= b.h * b.w - 1)
+      {
+        if (det[i].prob > det[j].prob)
+        {
+          det[j].prob = 0;
+        }
+        else
+        {
+          det[i].prob = 0;
+        }
+      }
+    }
+  }
+}
 
 template <typename T>
 T vectorProduct(const std::vector<T>& v)
@@ -121,8 +276,8 @@ int main(int argc, char* argv[])
   const double maskThreshold = 0.6;
 
   const std::string modelFilepath = "data/models/model.onnx";
-  const std::string imageFilepath = "data/images/test.jpg";
-  const std::string labelFilepath = "data/labels/COCO_classes.txt";
+  const std::string imageFilepath = "data/images/000000088462.jpg";
+  const std::string labelFilepath = "data/labels/coco.txt";
   const std::string instanceName = "image-classification-inference";
 
   const std::vector<std::string> labels = ReadLabels(labelFilepath);
@@ -130,12 +285,6 @@ int main(int argc, char* argv[])
   int imageWidth, imageHeight, imageChannels;
   stbi_uc * img_data = stbi_load(imageFilepath.c_str(), &imageWidth,
       &imageHeight, &imageChannels, STBI_default);
-
-  if (imageWidth != 1200 || imageHeight != 1200 || imageChannels != 3)
-  {
-    printf("Image size does't match with 416 x 416 x 3");
-    return 1;
-  }
 
   struct Pixel { unsigned char RGBA[3]; };
   const Pixel* imgPixels((const Pixel*)img_data);
@@ -179,47 +328,45 @@ int main(int argc, char* argv[])
       inputTypeInfo.GetTensorTypeAndShapeInfo().GetShape();
 
   // Change the first dimension from -1 to 1, necessary for Tiny YOLO v2.
-  if (inputDims[0] < 0)
-    inputDims[0] = 1;
+  inputDims[0] = 1;
+  inputDims[1] = imageHeight;
+  inputDims[2] = imageWidth;
+  inputDims[3] = 3;
+
   std::cout << "Input Dimensions: " << inputDims << std::endl;
 
   std::cout << "Output Name: " << std::endl;
   std::vector<const char*> outputNames;
-  for (size_t i = 0; i < session.GetOutputCount(); ++i)
-  {
-    std::cout << session.GetOutputName(i, allocator) << std::endl;
-    outputNames.push_back(session.GetOutputName(i, allocator));
-  }
+  /* for (size_t i = 0; i < session.GetOutputCount(); ++i) */
+  /* { */
+  /*   std::cout << session.GetOutputName(i, allocator) << std::endl; */
+  /*   outputNames.push_back(session.GetOutputName(i, allocator)); */
+  /* } */
+  outputNames.push_back("num_detections:0");
+  outputNames.push_back("detection_boxes:0");
+  outputNames.push_back("detection_scores:0");
+  outputNames.push_back("detection_classes:0");
 
   Ort::TypeInfo outputTypeInfo = session.GetOutputTypeInfo(0);
   auto outputTensorInfo = outputTypeInfo.GetTensorTypeAndShapeInfo();
 
-  std::vector<int64_t> outputDims = outputTensorInfo.GetShape();
-  // Change the first dimension from -1 to 1, necessary for Tiny YOLO v2.
-  if (outputDims[0] < 0)
-    outputDims[0] = 1;
-  std::cout << "Output Dimensions: " << outputDims << std::endl;
 
   // The model expects the image to be of size 416 x 416 x 3.
   const size_t inputTensorSize = imageWidth * imageHeight * imageChannels;
-  std::vector<float> inputTensorValues(inputTensorSize);
-
-  std::cout << inputTensorSize << std::endl;
+  std::vector<uint8_t> inputTensorValues(inputTensorSize);
 
   // Transpose image.
   Image *img = new Image(imageWidth, imageHeight, 3);
   size_t shift = 0;
-  std::vector<float> mean{0.485, 0.456, 0.406};
-  std::vector<float> std{0.229, 0.224, 0.225};
-  for (size_t c = 0; c < 3; ++c)
+  for (size_t y = 0; y < imageHeight; ++y)
   {
-    for (size_t y = 0; y < imageHeight; ++y)
+    for (size_t x = 0; x < imageWidth; ++x)
     {
-      for (size_t x = 0; x < imageWidth; ++x, ++shift)
+      for (size_t c = 0; c < 3; ++c, ++shift)
       {
         const int val(imgPixels[y * imageWidth + x].RGBA[c]);
         img->set((y * imageWidth + x) * 3 + c, val);
-        inputTensorValues[shift] = (val / 255 - mean[c]) / std[c];
+        inputTensorValues[shift] = (val);
       }
     }
   }
@@ -231,25 +378,27 @@ int main(int argc, char* argv[])
   Ort::MemoryInfo memoryInfo = Ort::MemoryInfo::CreateCpu(
       OrtAllocatorType::OrtArenaAllocator, OrtMemType::OrtMemTypeDefault);
 
-  inputTensors.push_back(Ort::Value::CreateTensor<float>(
+  inputTensors.push_back(Ort::Value::CreateTensor<uint8_t>(
       memoryInfo, inputTensorValues.data(), inputTensorSize, inputDims.data(),
       inputDims.size()));
 
   std::vector<Ort::Value> out = session.Run(Ort::RunOptions{nullptr},
       inputNames.data(), inputTensors.data(), 1, outputNames.data(), outputNames.size());
 
-  float* bboxes = out[0].GetTensorMutableData<float>();
-  int* labelsIndex = out[1].GetTensorMutableData<int>();
+  float* detections = out[0].GetTensorMutableData<float>();
+  float* bboxes = out[1].GetTensorMutableData<float>();
   float* scores = out[2].GetTensorMutableData<float>();
-  for (int i = 0; i < out[0].GetTensorTypeAndShapeInfo().GetShape()[1]; ++i)
+  float* labelsIndex = out[3].GetTensorMutableData<float>();
+
+  for (int i = 0; i < detections[0]; ++i)
   {
-    if (scores[i] > 0.1)
+    if (scores[i] > 0.3)
     {
       std::cout << "score: " << scores[i] << std::endl;
-      std::cout << "label: " << labelsIndex[i] << std::endl;
+      std::cout << "label: " << labels[labelsIndex[i] - 1] << std::endl;
       std::cout << "["
-                << bboxes[i * 4] << ", "
-                << bboxes[i * 4 + 1] << ", "
+                << bboxes[i * 4] * 1200 << ", "
+                << bboxes[i * 4 + 1]  * 1200 << ", "
                 << bboxes[i * 4 + 2] << ", "
                 << bboxes[i * 4 + 3]
                 << "]" << std::endl;
